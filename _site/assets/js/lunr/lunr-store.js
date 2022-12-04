c5b710e5890197a192dcde2eb4b52e587e64e244
Dennis Grothe <dennis.grothe@uni-bielefeld.de>
2022-12-04 16:37:08 +0100

    _site hinzugefügt

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Los gehts!",
+        "excerpt":"Herzlich Willkommen bei den Gesundheitsteilchen. Kommt herein, seht euch um und sucht euch was aus. Ich bin Dennis, euer Teilchenbäcker. Ich studiere  Health Communication und möchte hier mit den Gesundheitsteilchen kleine Storys und Projekte aus den Gesundheitswissenschaften vorstellen. Wo die Reise genau hingehen soll wird sich noch zeigen. Das wird...","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/los-gehts/",
+        "teaser": null
+      }]
